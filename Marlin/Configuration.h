4160388fd2208ee1c1db1b11ce671ab0d04513cd
Oral <33994774+oralallen82@users.noreply.github.com>
2020-07-08 20:25:00 -0500

    Enable Lin Advance and Zoffset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,7 +987,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -46, -12, -1.80} //OA Z -1.60 Glass Bed
+#define NOZZLE_TO_PROBE_OFFSET { -46, -12, -1.83} //OA Z -1.60 Glass Bed
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
