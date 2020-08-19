ee4aec557b1e0dadf170c0695f6e6c95b6a0e364
Oral <33994774+oralallen82@users.noreply.github.com>
2020-08-19 16:58:09 -0500

    Mods

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3153,7 +3153,7 @@
  *
  * Execute certain G-code commands immediately after power-on.
  */
-#define STARTUP_COMMANDS "M17 Z" //OA
+//#define STARTUP_COMMANDS "M17 Z" //OA
 
 /**
  * G-code Macros
