eda67bbf86e2eee96762f15e05439adfca34fe5c
Oral <33994774+oralallen82@users.noreply.github.com>
2021-03-12 09:13:23 -0600

    Oral Changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -565,7 +565,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 600
+#define EXTRUDE_MAXLENGTH 500
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
